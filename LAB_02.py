5aa12d819128da37030c97ef8f9c77b041b96481
„Dominik <dlucic@tvz.hr>
2023-03-20 13:42:53 +0100

    Labos 2

--- LAB_02.py
+++ LAB_02.py
@@ -1,1 +1,9 @@
 # LABORATORIJSKA VJEZBA 2
+
+kolegij = {}
+
+ime_kolegija = input("Unesite ime kolegija: ")
+broj_ECTS = int(input("Unesite ECTS bodove za kolegij: "))
+print("kolegij", ime_kolegija.upper(), "nosi", broj_ECTS, "ECTS bodova")
+
+
