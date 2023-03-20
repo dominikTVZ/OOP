bffccba25a5d5faf40e33e9041b29dc652e318b2
„Dominik <dlucic@tvz.hr>
2023-03-20 14:40:27 +0100

    labosi

--- LAB_02.py
+++ LAB_02.py
@@ -1,9 +1,12 @@
-# LABORATORIJSKA VJEZBA 2
+#LABORATORIJSKA VJEZBA 2
 
+from datetime import date
 kolegij = {}
 
 ime_kolegija = input("Unesite ime kolegija: ")
 broj_ECTS = int(input("Unesite ECTS bodove za kolegij: "))
 print("kolegij", ime_kolegija.upper(), "nosi", broj_ECTS, "ECTS bodova")
+
+ispit = {}
 
 
