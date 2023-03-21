dc1635d295e02e6ff952034d75bcbafdd28dc40f
„Dominik <dlucic@tvz.hr>
2023-03-21 13:40:36 +0100

    labosi 2

--- LAB_02.py
+++ LAB_02.py
@@ -1,12 +1,39 @@
-#LABORATORIJSKA VJEZBA 2
+# LABORATORIJSKA VJEZBA 2
 
 from datetime import date
-kolegij = {}
 
-ime_kolegija = input("Unesite ime kolegija: ")
-broj_ECTS = int(input("Unesite ECTS bodove za kolegij: "))
-print("kolegij", ime_kolegija.upper(), "nosi", broj_ECTS, "ECTS bodova")
+# Podaci o kolegiju
+ime_kolegija = input("Unesite ime kolegija: ").upper()
+ects_bodovi = int(input("Unesite ECTS bodove za kolegij: "))
 
-ispit = {}
+# Stvaranje rječnika za kolegij
+kolegij = {
+    "ime": ime_kolegija, "ects": ects_bodovi
+}
 
+# Podaci o ispitu
+dan = int(input("Unesite dan ispita: "))
+mjesec = int(input("Unesite mjesec ispita: "))
+godina = int(input("Unesite godinu ispita: "))
 
+# Stvaranje objekta datuma
+datum_ispita = date(godina, mjesec, dan)
+
+# Stvaranje rječnika za ispit
+ispit = {
+    "kolegij": kolegij, "datum": datum_ispita
+}
+
+# Podaci o studentu
+ime_studenta = input("Unesite ime studenta: ").capitalize()
+prezime_studenta = input("Unesite prezime studenta: ").capitalize()
+
+# Stvaranje rječnika za studenta
+student = {
+    "ispit": ispit, "ime": ime_studenta, "prezime": prezime_studenta
+}
+
+# Ispis
+print("Student", ime_studenta, prezime_studenta)
+print("je prijavio ispit iz kolegija", student["ispit"]["kolegij"]["ime"])
+print("koji će se održati:", student["ispit"]["datum"])
