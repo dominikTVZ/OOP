4794946056c8f5fe0be26a93cc71dad48df56666
Dominik Lučić (dlucic) <dlucic@tvz.hr>
2023-03-28 16:43:12 +0200

    vjezbanje

--- LAB_03.py
+++ LAB_03.py
@@ -1,39 +1,40 @@
-"""""
-zivotinje = []
+from datetime import date
+kolegiji = []
 
-broj_zivotinja = int(input("Unesite broj životinja: "))
+broj_kolegija= int(input("Unesite broj kolegija: "))
 
-for i in range(broj_zivotinja):
-    zivotinja = {}
+for i in range(1, broj_kolegija+1):
+    kolegij = {}
 
-    zivotinja["ime"] = input("Unesite ime zivotinje: ")
-    zivotinja["vrsta"] = input("Unesite vrstu zivotinje: ")
+    kolegij['ime'] = input(f"Unesite ime {i}. kolegija: ")
+    kolegij['ECTS'] = int(input(f"Unesite broj ects-a {i}. kolegija: "))
 
-    zivotinje.append(zivotinja)
-
-for zivotinja in zivotinje:
-    print(zivotinja["ime"])
-   
-
-zivotinje = ["Dino", "Kira", "Šnjofavac"]
-
-for index, zivotinja in enumerate(zivotinje, start = 1):
-    print(f'{index}. {zivotinja}')
+    kolegiji.append(kolegij)
 
 
-korisnik = {
-    "ime": "Ivan",
-    "prezime":"Ivic",
-    "godine":24
-}
-for value in korisnik:
-    print(value, end = " ")
-    print()
+ispiti = []
 
-for key, value in korisnik.items():
-    print(f"{key}{value}", end = " ")
-    print()
-for value in korisnik.values():
-    print(f"{value}",end = "")
-    print()
-"""""
+broj_ispita = int(input("Unesite broj ispita: "))
+
+for i in range(1, broj_ispita+1):
+    ispit = {}
+
+    for j, kolegij in enumerate(kolegiji, start = 1):
+        print(f"\t{j}. {kolegij['ime']}")
+
+    odabrani_kolegij = int(input("Odaberite kolegij: "))
+
+    ispit['kolegij'] = kolegiji[odabrani_kolegij-1]
+
+
+    dan = int(input(f"Unesite dan {i}. ispita: "))
+    mjesec = int(input(f"Unesite mjesec {i}. ispita: "))
+    godina = int(input(f"Unesite godinu {i}. ispita: "))
+
+    ispit["datum"] = date(godina, mjesec, dan)
+    ispiti.append(ispit)
+
+    kolegiji.append(kolegij)
+
+    print("Popis svih ispita: ")
+    print(f"\t Ispit iz kolegija ")
