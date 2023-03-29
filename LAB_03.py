da9425c85d20e3009b8df80ee55fd49a275c28dd
„Dominik <dlucic@tvz.hr>
2023-03-29 15:19:24 +0200

    Labos 3

--- LAB_03.py
+++ LAB_03.py
@@ -16,25 +16,42 @@ ispiti = []
 
 broj_ispita = int(input("Unesite broj ispita: "))
 
+#ISPIT JE PREDSTAVLJEN RIJECNIKOM KOJI SADRZI KOLEGIJ(IME,ECTS) I DATUM
 for i in range(1, broj_ispita+1):
     ispit = {}
 
+    print("Popis kolegija: ")
     for j, kolegij in enumerate(kolegiji, start = 1):
         print(f"\t{j}. {kolegij['ime']}")
 
-    odabrani_kolegij = int(input("Odaberite kolegij: "))
+    odabrani_kolegij = int(input("Unesite kolegij: "))
 
     ispit['kolegij'] = kolegiji[odabrani_kolegij-1]
-
 
     dan = int(input(f"Unesite dan {i}. ispita: "))
     mjesec = int(input(f"Unesite mjesec {i}. ispita: "))
     godina = int(input(f"Unesite godinu {i}. ispita: "))
+    ispit['datum'] = date(godina, mjesec, dan)
 
-    ispit["datum"] = date(godina, mjesec, dan)
     ispiti.append(ispit)
 
-    kolegiji.append(kolegij)
+"""ISPIS ZA 2.ZADATAK
+print("Popis svih ispita: ")
+for ispit in ispiti:
+    print(f"\t Ispit iz kolegija {ispit['kolegij']['ime']},")
+    print(f"\tkoji nosi {ispit['kolegij']['ECTS']} bodova održat će se {ispit['datum']}")
+"""
 
-    print("Popis svih ispita: ")
-    print(f"\t Ispit iz kolegija ")
+studenti = []
+
+broj_studenta= int(input("Unesite broj studenta: "))
+for i in range(1, broj_studenta+1):
+    student = {}
+
+    for j,
+
+        student['ime'] = input(f"Unesite ime {i}. kolegija: ")
+        student['prezime'] = int(input(f"Unesite broj ects-a {i}. kolegija: "))
+
+        studenti.append(student)
+
